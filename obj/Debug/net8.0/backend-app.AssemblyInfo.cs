2b370c28af0440ee90775744867c97bd4f4dda9d
muyi <eroosamuyi11451@gmail.com>
2025-02-03 02:09:46 +0000

    removed the git workflow file

--- obj/Debug/net8.0/backend-app.AssemblyInfo.cs
+++ obj/Debug/net8.0/backend-app.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend-app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebee26e0479a696c2d372c45ebe606987d00348d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bdb4990e97626a42de06b1a126375c4c99e9130c")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend-app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend-app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
