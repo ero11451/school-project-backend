125cd124fb5494c2600fca2798e0e6962b7ff5ec
muyi <eroosamuyi11451@gmail.com>
2025-02-03 14:37:21 +0000

    addeded the public assect

--- obj/Debug/net8.0/backend-app.AssemblyInfo.cs
+++ obj/Debug/net8.0/backend-app.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend-app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+109e88002a2b4f6f36a4e5cb41fcc58bd8d1597a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32ac5f962c3c31d57a40da157beaa71d59dbc3d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend-app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend-app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
